8d6f7f361b5ab3e43bfde1593e40c01418d33ca0
Samuel Getachew <samget78@gmail.com>
2017-12-28 09:41:49 +0300

    Modified the font color, nav background and added logo icon image

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -10,7 +10,7 @@ main_page_head = '''
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
-
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">.
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
@@ -25,7 +25,7 @@ main_page_head = '''
             width: 640px;
             height: 480px;
         }
-            .navbar {
+        .navbar {
             # background: #0294c9;
             background: #016697;
             border: none;
@@ -64,9 +64,13 @@ main_page_head = '''
             top: 0;
             background-color: white;
         }
-        .logo {
-            fill: white;
+        @media screen and (max-width: 780px) {
+            .poster-image {
+                width: 100%;
+                height: 100%;
+            }
         }
+
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
@@ -134,7 +138,7 @@ main_page_content = '''
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
+    <img src="{poster_image_url}" width="220" height="342" class="poster-image">
     <h2>{movie_title}</h2>
 </div>
 '''
