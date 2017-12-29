6a3683194e96319d5e1ccee116bda346d6ccc540
Samuel Getachew <samget78@gmail.com>
2017-12-29 13:13:28 +0300

    Added comments and format code according to PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,64 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that came to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+# Creating movies based on the instances of the Class Movie from module media
 
-avatar = media.Movie("Avatar",
-                     "A marine on alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "http://youtube.com/watch?v=-9ceBgWV8io")
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that came to life",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4"
+)
 
-the_last_jedi = media.Movie("Star Wars: The Last Jedi",
-                            "Rey develops her newly discovered abilities with the guidance of Luke Skywalker",
-                            "https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg",
-                            "https://www.youtube.com/watch?v=Q0CbN8sfihY")
+avatar = media.Movie(
+    "Avatar",
+    "A marine on alien planet",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "http://youtube.com/watch?v=-9ceBgWV8io"
+)
 
-shawshank = media.Movie("The Shawshank Redemption",
-                        "Banker, sentenced to life for a murder, despite his claims of innocence",
-                        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                        "https://www.youtube.com/watch?v=6hB3S9bIaco")
+the_last_jedi = media.Movie(
+    "Star Wars: The Last Jedi",
+    """Rey develops her newly discovered abilities with the "
+    guidance of Luke Skywalker""",
+    "https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg",  # Noqa
+    "https://www.youtube.com/watch?v=Q0CbN8sfihY")
 
-arrival = media.Movie("Arrival",
-                      "A linguist, enlisted by the Army to help translate communications from extraterrestrial craft.",
-                      "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",
-                      "https://www.youtube.com/watch?v=tFMo3UJ4B4g")
+shawshank = media.Movie(
+    "The Shawshank Redemption",
+    "Banker, sentenced to life for a murder, despite his claims of innocence",
+    "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # Noqa
+    "https://www.youtube.com/watch?v=6hB3S9bIaco"
+)
 
-wonder = media.Movie("Wonder",
-                     "A kid who has a rare medical facial deformity",
-                     "https://upload.wikimedia.org/wikipedia/en/6/67/Wonder_%28film%29.png",
-                     "https://www.youtube.com/watch?v=ngiK1gQKgK8")
+arrival = media.Movie(
+    "Arrival",
+    """A linguist, enlisted by the Army to help translate communications from
+    extraterrestrial craft.""",
+    "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",  # Noqa
+    "https://www.youtube.com/watch?v=tFMo3UJ4B4g"
+)
 
-movies = [arrival, wonder, avatar, the_last_jedi, shawshank]
+wonder = media.Movie(
+    "Wonder",
+    "A kid who has a rare medical facial deformity",
+    "https://upload.wikimedia.org/wikipedia/en/6/67/Wonder_%28film%29.png",
+    "https://www.youtube.com/watch?v=ngiK1gQKgK8"
+)
+
+# Movies array, contains the list of movies as input, which
+# is passed to the function "open_movies_page". This function
+# translates this list into a web page when we run the
+# "entertainment_center.py" file.
+
+movies = [
+    arrival,
+    wonder,
+    avatar,
+    the_last_jedi,
+    shawshank
+]
+
+
 fresh_tomatoes.open_movies_page(movies)
 print(media.Movie.VALID_RATINGS)
--- media.py
+++ media.py
@@ -1,3 +1,4 @@
+""" Module to display movie object, attributes and instances """
 import webbrowser
 
 
@@ -5,11 +6,25 @@ class Movie(object):
     """This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """
+        Intialize instance of class Movie
+
+        Args:
+            movie_title (str): Title of the Movie.
+            movie_storyline (str): Short description about the movie.
+            poster_image (str): Movie Poster Image URL.
+            trailer_youtube (str): Movie trailer URL.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        Returns:
+            webbrowser To play the trailer with your default browser
+        """
         webbrowser.open(self.trailer_youtube_url)
